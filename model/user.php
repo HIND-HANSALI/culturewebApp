5868992c50e706e2056fac14b8251bd4243b7d19
HIND-HANSALI <hindhsl111@gmail.com>
2023-01-16 21:43:24 +0100

    create login fct model

--- model/user.php
+++ model/user.php
@@ -37,7 +37,44 @@ class User extends Database{
         return $resultCheck;
     }
 
- 
+    /* ============================== Login ============================== */
+    protected function login($email, $password){
+        $stmt = $this->connect()->prepare('SELECT * FROM admins WHERE email = ?;');
+
+        if (!$stmt->execute(array($email))) {
+            $stmt = null;
+            header("location : ../pages/login.php?error=stmtfailed");
+            exit();
+        }
+
+
+        if ($stmt->rowCount() == 0) { 
+            // $stmt = null;
+            // $_SESSION["email"] = $email;
+            // $_SESSION["password"] = $password;
+            header("location: ../pages/login.php?error=wronglogin");
+            exit();
+        }
+        //check password if its the same
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        $checkPwd = password_verify($password, $result["password"]);
+
+        if ($checkPwd == false) {
+            $stmt = null;
+            $_SESSION["email"] = $email;
+            $_SESSION["password"] = $password;
+            header("location: ../pages/login.php?error=wronglogin");
+            exit();
+        } elseif ($checkPwd == true) {
+            $_SESSION["name"] = $result["username"];
+            // $_SESSION["id"] =  $result["id_user"];
+            // $_SESSION["role"] =  $result["id_role"];
+            $_SESSION["email"] = $email;
+            // $_SESSION["good"] = "goode";
+        }
+        $stmt = null;
+    }
  
 
 
