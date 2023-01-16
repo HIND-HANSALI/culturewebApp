e8fe4876e05e2831385f369835a1e937cfc4b1c1
HIND-HANSALI <hindhsl111@gmail.com>
2023-01-16 14:45:03 +0100

    create connection

---
+++ model/database.php
@@ -0,0 +1,36 @@
+<?php
+class Database{
+  private $host;
+  private  $db_name;
+  private  $username;
+  private  $password;
+  private  $conn;
+  // contructor to set database credentials
+  public function __construct(){
+      $this->host = "localhost";
+      $this->db_name = "culturedev";
+      $this->password = '';
+      $this->username = 'root';
+  }
+  // database connection
+  public function connect(){
+      // $this->conn = null;
+      try {
+          $conn = new PDO("mysql:host=$this->host;dbname=$this->db_name", $this->username, $this->password);
+          // set the PDO error mode to exception
+          $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          // echo "Connected successfully";
+          return $conn;
+        } catch(PDOException $e) {
+          echo "Connection failed: " . $e->getMessage();
+          die;
+        }
+    }
+}
+
+// $test=new Database();
+// $test->connect();
+
+
+
+?>
