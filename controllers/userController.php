4b6e70330c2f41f1f7926681a097a05fd32862d7
HIND-HANSALI <hindhsl111@gmail.com>
2023-01-20 19:52:05 +0100

    add fct getAdmins & Logout

--- controllers/userController.php
+++ controllers/userController.php
@@ -1,53 +1,52 @@
 <?php 
 
-include_once '../model/user.php';
+include_once dirname(__DIR__).'/model/user.php';
+
 
 class UserController extends User{
 
-
-
-
       /* ============================== SignUp ============================== */
     public function signupUser(){
-        // echo 'hiii';
-        // if($_SERVER['REQUEST_METHOD'] == 'POST'){
-            if(isset($_POST['signup'])){
-                echo 'hiii';
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            if(isset($_POST['signup'])){             
+                extract($_POST);
 
-                // extract($_POST);
-
-                // echo $_SESSION["name"] = $userName;
-                // echo $_SESSION["email"] = $email;
-                // echo $_SESSION["password"] = $password;
-                // echo $_SESSION["Rpassword"] = $passwordCheck;
-                // $this->validateSignup($userName,$email,$password,$passwordCheck);
-                // echo "hhh";
-
+                echo $_SESSION["name"] = $userName;
+                echo $_SESSION["email"] = $email;
+                echo $_SESSION["password"] = $password;
+                echo $_SESSION["Rpassword"] = $passwordCheck;
+                $this->validateSignup($userName,$email,$password,$passwordCheck);
             }
-        // }
+        }
     }
 
     public function validateSignup($name,$email,$password,$RepeatPassword){
         if(!$this->emptyInputSignup($name, $email, $password, $RepeatPassword)){
+            $_SESSION['error'] = 'Inputs are required';
             header("location: ../pages/signup.php?error=emptyinput");
             exit();
         }
         elseif(!$this->invalidName($name)){
+            $_SESSION['error'] = 'Provide a valid username';
             header("location: ../pages/signup.php?error=Errorusername");
             exit();
         }
         elseif(!$this->invalidEmail($email)){
+            $_SESSION['error'] = 'Provide a valid email';
             header("location: ../pages/signup.php?error=Erroremail");
             exit();
         }
         elseif(!$this->passwordMatch($password, $RepeatPassword)){
+            $_SESSION['error'] = 'Passwords doesnt match';
             header("location: ../pages/signup.php?error=Matchpassword");
             exit();
         }
         elseif(!$this->checkEmailSignup($email)){
+            $_SESSION['error'] = 'This email already exist';
             header("location: ../pages/signup.php?error=emailtaken");
             exit();
         }else{
+            
             $this->signUp($name, $email, $password);
             header('location: ../pages/login.php');
         }
@@ -59,7 +58,6 @@ class UserController extends User{
         // if($_SERVER['REQUEST_METHOD'] == 'POST'){
             if(isset($_POST['login'])){
 
-
                 extract($_POST);
                 $this->validateLogin($email,$password);
 
@@ -70,14 +68,20 @@ class UserController extends User{
     public function validateLogin($email,$password){
 
         if(!$this->emptyInputLogin($email, $password)){
+            $_SESSION['error'] = 'Inputs are required';
             header("location: ../pages/login.php?error=emptyinput");
             exit();
         }elseif(!$this->invalidEmail($email)){
+            $_SESSION['error'] = 'Provide a valid email';
             header("location: ../pages/login.php?error=Erroremail");
             exit();
         }else{
             $this->login($email, $password);
-            header('location: ../index.php');
+            // die;
+            // $_SESSION['good'];
+            // header('location: pages/post.php');
+            // header('location: ../pages/post.php');
+
         }
     }
 
@@ -147,6 +151,23 @@ class UserController extends User{
 
      return $result;
  }
+
+
+
+
+
+    public function getAdmins(){
+        $result = $this -> getAdminsDB();
+            return $result;
+
+    }
+    public function logout(){
+        if(isset($_POST['logout'])){
+        $this -> logoutDB();
+
+            
+        }
+}
 
 }
 
